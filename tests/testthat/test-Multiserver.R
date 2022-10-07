67c049091315f18e2902501c6d6295077c2d5553
Charles Joseph <charles.joseph@students.mq.edu.au>
2022-10-08 02:32:39 +1100

    Initial commit for multiserver package

--- tests/testthat/test-Multiserver.R
+++ tests/testthat/test-Multiserver.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("test multiserver works", {
+  expect_identical(Multiserver(1,1,1), dplyr::tibble(
+    Arrivals = 1,
+    ServiceBegins =1,
+    ChosenServer = 1,
+    ServiceEnds = 2
+  ))
+  expect_error(Multiserver())
 })
