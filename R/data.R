a34cddb211d1733c49044003bc71717b53448d0c
Charles Joseph <charles.joseph@students.mq.edu.au>
2022-10-12 23:54:56 +1100

    Changing variable name within my bank dataset script

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Bank Data
+#'
+#' simulates the comings and goings of customers through the system, along with the time when each customer will arrive and their service times.
+#'
+#' Report ...
+#'
+#' A data frame with 100 rows and 2 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.npmjs.com/package/multiserver>
+"bank"
--- tests/testthat/test-Multiserver.R
+++ tests/testthat/test-Multiserver.R
@@ -1,4 +1,4 @@
 test_that("test multiserver works", {
-  expect_equal(Multiserver(1,1,1), data.frame(Arrivals = 1, ServiceBegins =1, ChosenServer = 1, ServiceEnds = 2))
+  expect_equal(Multiserver(1,1,1), dplyr::tibble(Arrivals = 1, ServiceBegins =1, ChosenServer = 1, ServiceEnds = 2))
   expect_error(Multiserver())
 })
