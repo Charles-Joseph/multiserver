e904663ddf428f89d533c2bdb425066bc785d406
Charles Joseph <charles.joseph@students.mq.edu.au>
2022-10-12 23:27:09 +1100

    Description changed when importing dplyr

--- tests/testthat/test-Multiserver.R
+++ tests/testthat/test-Multiserver.R
@@ -1,9 +1,4 @@
 test_that("test multiserver works", {
-  expect_identical(Multiserver(1,1,1), dplyr::tibble(
-    Arrivals = 1,
-    ServiceBegins =1,
-    ChosenServer = 1,
-    ServiceEnds = 2
-  ))
+  expect_equal(Multiserver(1,1,1), dplyr::tibble(Arrivals = 1, ServiceBegins =1, ChosenServer = 1, ServiceEnds = 2))
   expect_error(Multiserver())
 })
